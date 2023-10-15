ad02bef1dc66e4e3bdf126e815a71fa480371424
Tousif Ahmed <tousifkhan510@gmail.com>
2023-10-15 22:08:53 +0530

    NavBar styling

---
+++ app/NavBar.tsx
@@ -0,0 +1,41 @@
+'use client';
+
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+import React from 'react'
+import { AiFillBug } from 'react-icons/ai'
+import classnames from 'classnames';
+
+const NavBar = () => {
+
+    const currentPath = usePathname();
+
+    const links = [
+        { label: 'Dashboard', href: '/' },
+        { label: 'Issues', href: '/issues' }
+    ];
+
+  return (
+    <nav className='flex space-x-6 border-b mb-10 px-5 h-14 items-center'>
+        <Link href="/"><AiFillBug /></Link>
+        <ul className='flex space-x-6'>
+            { links.map (link => 
+                <li>
+                    <Link 
+                        key={link.href}
+                        href={link.href} 
+                        className={classnames({
+                            'text-zinc-900': link.href === currentPath,
+                            'text-zinc-500': link.href !== currentPath,
+                            'hover:text-zinc-900 transition-colors': true
+                        })}>
+                            {link.label}
+                    </Link>
+                </li>) 
+            }
+        </ul>
+    </nav>
+  )
+}
+
+export default NavBar
