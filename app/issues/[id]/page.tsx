21877a32cbccaeef42c26e0edd2e037faa71fd95
Tousif Ahmed <tousifkhan510@gmail.com>
2023-10-17 23:33:23 +0530

    Markdown editor on Edit

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -2,8 +2,7 @@ import IssueStatusBadge from '@/app/components/IssueStatusBadge'
 import prisma from '@/prisma/client'
 import { Card, Flex, Heading, Text } from '@radix-ui/themes'
 import { notFound } from 'next/navigation'
-import React from 'react'
-
+import ReactMarkdown from 'react-markdown'
 interface Props {
     params: { id: string }
 }
@@ -26,9 +25,8 @@ const IssueDetailPage = async ({ params }: Props) => {
             <Text>{issue.createdAt.toDateString()}</Text>
         </Flex>
         <Card className='mt-5'>
-            <p>{issue.description}</p>
+            <ReactMarkdown className='prose-slate'>{issue.description}</ReactMarkdown>
         </Card>
-        
     </div>
   )
 }
