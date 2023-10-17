7142ea5f872d4cf43d586009f18519ad3ef256e1
Tousif Ahmed <tousifkhan510@gmail.com>
2023-10-17 21:00:35 +0530

    Loading Sketelon

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -3,15 +3,17 @@ import { Button, Table } from '@radix-ui/themes'
 import Link from 'next/link'
 import prisma from '@/prisma/client'
 import IssueStatusBadge from '../components/IssueStatusBadge'
+import delay from 'delay'
+import IssueActions from './IssueActions'
 
 const IssuePage = async () => {
   const issues = await prisma.issue.findMany();
 
+  await delay(1000); //2 sec
+
   return (
     <div>
-      <div className='mb-5'>
-        <Button><Link href='/issues/new'>New Issue</Link></Button>
-      </div>
+      <IssueActions />
       <Table.Root variant="surface">
         <Table.Header>
           <Table.Row>
@@ -22,7 +24,7 @@ const IssuePage = async () => {
         </Table.Header>
         <Table.Body>
           {issues.map(issue => (
-            <Table.Row>
+            <Table.Row key={issue.id}>
               <Table.Cell>{issue.title}</Table.Cell>
               <Table.Cell><IssueStatusBadge status={issue.status} /></Table.Cell>
               <Table.Cell>{issue.createdAt.toDateString()}</Table.Cell>
