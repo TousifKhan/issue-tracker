4d066d78f14b8d463223db01dbf20cb957c18b09
Tousif Ahmed <tousifkhan510@gmail.com>
2023-10-18 10:21:16 +0530

    Fix UI issue + Optimize import

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,9 +1,9 @@
-import { Table } from '@radix-ui/themes'
 import prisma from '@/prisma/client'
-import IssueStatusBadge from '../components/IssueStatusBadge'
+import { Table } from '@radix-ui/themes'
 import delay from 'delay'
-import IssueActions from './IssueActions'
+import IssueStatusBadge from '../components/IssueStatusBadge'
 import Link from '../components/Link'
+import IssueActions from './IssueActions'
 
 const IssuePage = async () => {
   const issues = await prisma.issue.findMany();
