7e4f7f136f42e70c930d846058e0a29da83e1aa9
Tousif Ahmed <tousifkhan510@gmail.com>
2023-10-20 11:55:14 +0530

    Delete Issue endpoint

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -1,5 +1,6 @@
 import { issueSchema } from "@/app/ValidationSchema";
 import prisma from "@/prisma/client";
+import { error } from "console";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function PATCH(
@@ -29,4 +30,21 @@ export async function PATCH(
   });
 
   return NextResponse.json(updatedIssue, { status: 200 });
+}
+
+export async function DELETE(
+  req: NextRequest,
+  { params }: { params: { id: string } },
+) {
+  const issue = await prisma.issue.findUnique({
+    where: {
+      id: parseInt(params.id, 10),
+    },
+  });
+  if (!issue)
+    return NextResponse.json({ error: "Invalid Issue." }, { status: 404 });
+
+  await prisma.issue.delete({ where: { id: parseInt(params.id, 10) } });
+
+  return NextResponse.json({}, { status: 200 });
 }
